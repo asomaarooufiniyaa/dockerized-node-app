a09b9848526bf334e0255efb2f0ab73229d57706
asomaarooufiniyaa <asomaarooufiniyaa@gmail.com>
2025-09-18 12:05:12 +0330

    config file added

---
+++ config/config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    mongo_ip : process.env.MONGO_IP || "mongo",
+    mongo_port : process.env.MONGO_PORT || "27017",
+    mongo_user : process.env.MONNGO_USER || "aso",
+    mongo_password : process.env.MONGO_PASSWORD || "aso20017701",
+};
